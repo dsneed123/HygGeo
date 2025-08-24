b14897f16bbbb5e5a8d0cd591d1362c4eb695f1b
Davis Sneed <dlsneed1298@gmail.com>
2025-08-23 20:28:32 -0700

    db reset

--- HygGeo/HygGeo/settings.py
+++ HygGeo/HygGeo/settings.py
@@ -37,6 +37,8 @@ if DEBUG:
     CSRF_TRUSTED_ORIGINS = [
         'http://localhost:8000',
         'http://127.0.0.1:8000',
+        'https://hyggeo.com',
+        'https://www.hyggeo.com',
     ]
 else:
     CSRF_TRUSTED_ORIGINS = [
